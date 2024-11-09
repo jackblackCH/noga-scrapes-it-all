d367fda3ba1c89f4fb8b057f44b2e244c8eb1bfc
Marc Illien <marc.illien@gmail.com>
2024-11-09 04:34:21 +0100

    No revalidate

--- app/api/companies/route.ts
+++ app/api/companies/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import Airtable, { FieldSet, Record } from 'airtable';
 import { Job } from '@/app/types/job';
 
-export const revalidate = 5;
+export const revalidate = false;
 
 interface AirtableFields extends FieldSet {
   Company?: string;
